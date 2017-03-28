06ce8b83d5e1617d2c77a7ee5915cb616a641d4f
Alex Gray <agray@clearcareonline.com>
2017-03-28 16:08:19 -0400

    WIP add demo_02.py

--- check_spot_instance_termination.sh
+++ check_spot_instance_termination.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
-if curl -s --max-time 5 http://169.254.169.254/latest/meta-data/spot/termination-time | grep -q .*T.*Z; then
+URL=http://169.254.169.254/latest/meta-data/spot/termination-time
+if curl -s --max-time 5 ${URL} | grep -q .*T.*Z; then
   echo "spot instance termination imminent"
   # Create a file that Jenkins will archive so that the caller of this job can
   # detect this job needs to be re-kicked:
--- demo_02.py
+++ demo_02.py
@@ -73,10 +73,59 @@ def driver(test_file):
                 # It takes one more poll to get the results:
                 r.poll()
                 say('Build Done: {0} status: {1}'.format(r.baseurl, r.get_status()))
+                say('Downloading artifacts...')
+                artifact_dict = r.get_artifact_dict()
+                if 'spot_instance_termination_imminent.txt' in artifact_dict:
+                    say('spot_instance_termination_imminent.txt in artifact. We must kick off new worker...')
+                    # Uh-oh. Worker died due to spot instance dying.
+                    # re-kick this job and wait till it's off the queue and building:
+                    actions = r.get_data(r.baseurl + '/api/python')['actions']
+                    params = {}
+                    for action in actions:
+                        if 'parameters' in action:
+                            for param in action['parameters']:
+                                params[param['name']] = param['value']
+                        break
+                    # Put this jobs on the queue:
+                    q = build_job(host=args.jenkins_host,
+                                  build_name=args.unit_test_driver,
+                                  params=params,
+                                  username=args.jenkins_username,
+                                  password=args.jenkins_password)
+                    # and wait for it to get off the queue:
+                    while True:
+                        try:
+                            q.poll()
+                            results.append(q.get_build())
+                            update_in_progress_file(aStr=None, q=q)
+                            break
+                        except NotBuiltYet:
+                            say('still on the queue: {0}'.format(q))
+                            time.sleep(15)
+                            continue
+                        except requests.exceptions.HTTPError as ex:
+                            # Edge case where queue item is off the queue
+                            # and has been deleted by Jenkins
+                            if ex.response.status_code == 404:
+                                say('breaking out of loop for this item on the queue...')
+                                break
+                else:
+                    # The worker is done:
+                    for artifact_name, v in artifact_dict.items():
+                        say('Saving artifact to: {0}'.format(os.path.join(args.artifact_dir, artifact_name)))
+                        for i in range(5):
+                            try:
+                                v.save(os.path.join(args.artifact_dir, artifact_name), strict_validation=False)
+                                break
+                            except Exception as ex:
+                                say(ex)
+                                say('Carry on despite this exception...')
+                                time.sleep(3)
                 # Regardless of what happened, the worker is done, so
                 # take it off the list
                 if r.get_status() != 'SUCCESS':
                     ret_code += 1
+
                 master_results.append(r)
                 results.remove(r)
         time.sleep(30)
